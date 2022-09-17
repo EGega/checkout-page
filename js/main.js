67e45c6fb9e7c9a2eeb1d31ffa9bda029df53d76
gegaenea@yahoo.com <gegaenea@yahoo.com>
2022-09-17 23:57:04 +0300

    selectors

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const plusBtn = document.querySelectorAll(".plus");
+const minusBtn = document.querySelectorAll(".minus");
+const removeBtn = document.querySelectorAll(".remove");
+const salePrice = document.querySelectorAll(".orange");
+const originalPrice = document.querySelectorAll(".overline");
+const cartItem = document.querySelectorAll(".cart-item")
+
+// ALT Selectorlar
+const subTotal = document.querySelector("subtotal")
+const vat = document.getElementById("vat")
+const shipping = document.getElementById("shipping")
+const sumPrice = document.getElementById("sum-price")
+
