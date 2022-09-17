d71616630894d39fe8af375cc4f94f884283f2ab
gegaenea@yahoo.com <gegaenea@yahoo.com>
2022-09-18 02:44:12 +0300

    Plus Func added

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const plusBtn = document.querySelectorAll(".plus");
 const minusBtn = document.querySelectorAll(".minus");
 const removeBtn = document.querySelectorAll(".remove");
 const salePrice = document.querySelectorAll(".orange-span");
-const originalPrice = document.querySelectorAll(".overline");
+const originalPrice = document.querySelectorAll(".overline-span");
 const cartItem = document.querySelectorAll(".cart-item");
 const number = document.querySelectorAll(".number")
 
@@ -23,25 +23,54 @@ removeBtn.forEach((btn) => {
 
 
 // Ekleme 
-const doubleAdd = () => {
-  salePrice.forEach((sale) => {
-    sale.innerText = "Hello";
-  })
-}
+// const doubleAdd = () => {
+//   salePrice.forEach((sale) => {
+//     sale.innerText = "Hello";
+//   })
+// }
+// let saleNum = nu 
 
-plusBtn.forEach((plus) => {
-  plus.addEventListener("click", (e) => { 
-   
+// plusBtn.forEach((btn, index) => {
+
+//   btn.addEventListener("click", () => {
+//       number[index].innerHTML = (parseInt(number[index].innerHTML) + 1);
+       
+//       saleNum = parseFloat(salePrice[index].innerHTML)
+
+//       salePrice[index].innerHTML = (parseFloat(salePrice[index].innerHTML) + saleNum).toFixed(2);
+
+//       originalPrice[index].innerHTML = (parseFloat(originalPrice[index].innerHTML) + parseFloat(originalPrice[index].innerHTML)).toFixed(2);
+//   })
+// }
+// )
+
+
+
+plusBtn.forEach((btn, i) => {
+  let saleNum = Number(salePrice[i].innerHTML);
+  const saleNumCopy = saleNum
+  let orgNum = Number(originalPrice[i].innerHTML);
+  const orgCopy = orgNum
+  btn.addEventListener("click", () => {
+    number[i].innerHTML = (parseInt(number[i].innerHTML) + 1);
+    saleNum += saleNumCopy
+    salePrice[i].innerHTML = saleNum
+    orgNum += orgCopy
+    originalPrice[i].innerHTML = orgNum
   })
 })
-
-//   e.target.parentElement.parentElement.parentElement.children[1].innerHTML = `$ <span class="orange-span">54.99</span>`
-  //  let salePriceValue = e.target.parentElement.parentElement.parentElement.children[1].lastChild.innerText;
-  //  e.target.parentElement.parentElement.parentElement.children[1].innerHTML = `$ <span class="orange-span">${salePriceValue *= 2 }</span>`
-   // console.log(salePriceValue);
-
-
-
     
 
+// plusBtn.forEach((btn, index) => {
+//   btn.addEventListener("click", () => (plusButtonHandler(index)))
+// }
+// )
+
+// const plusButtonHandler = (index) => {
+//   saleNumber = parseFloat(salePrice[index].innerHTML);
+//   originaNumber = parseFloat(originalPrice[index].innerHTML);
+//   number[index].innerHTML = (parseInt(number[index].innerHTML) + 1);
+//   salePrice[index].innerHTML = (parseFloat(salePrice[index].innerHTML) + saleNumber).toFixed(2);
+//   originalPrice[index].innerHTML = (parseFloat(originalPrice[index].innerHTML) + originaNumber).toFixed(2);
+// }
 
