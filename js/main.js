c4e8e627a0b2d5e37ed0fe17a2bf2804f4eb73a6
gegaenea@yahoo.com <gegaenea@yahoo.com>
2022-09-18 00:25:09 +0300

    RemoveFunction

--- js/main.js
+++ js/main.js
@@ -3,11 +3,24 @@ const minusBtn = document.querySelectorAll(".minus");
 const removeBtn = document.querySelectorAll(".remove");
 const salePrice = document.querySelectorAll(".orange");
 const originalPrice = document.querySelectorAll(".overline");
-const cartItem = document.querySelectorAll(".cart-item")
+const cartItem = document.querySelectorAll(".cart-item");
+const number = document.querySelectorAll(".number")
 
 // ALT Selectorlar
 const subTotal = document.querySelector("subtotal")
 const vat = document.getElementById("vat")
 const shipping = document.getElementById("shipping")
 const sumPrice = document.getElementById("sum-price")
+
+// RemoveBtn Function
+
+removeBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+     btn.parentElement.parentElement.remove()
+  });
+});
+
+
+    
+
 
